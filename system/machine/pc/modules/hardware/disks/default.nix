4c8d661597edbc11ac7b0717211b8adba110036b
llohatca <llohatca@gmail.com>
2025-01-10 23:20:44 +0500

    system/machine/pc/modules/hardware/disks: Fix disks
    Signed-off-by: llohatca <llohatca@gmail.com>

--- system/machine/pc/modules/hardware/disks/default.nix
+++ system/machine/pc/modules/hardware/disks/default.nix
@@ -1,14 +1,37 @@
 {
   fileSystems."/" =
     {
-      device = "/dev/sda2";
-      fsType = "ext4";
+      device = "/dev/sda1";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
     };
 
-  fileSystems."/boot" =
+
+  fileSystems."/home" =
     {
       device = "/dev/sda1";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
+
+  fileSystems."/var/log" =
+    {
+      device = "/dev/sda1";
+      fsType = "btrfs";
+      options = [ "subvol=@log" ];
+    };
+
+  fileSystems."/nix" =
+    {
+      device = "/dev/sda1";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" ];
+    };
+
+  fileSystems."/.snapshots" =
+    {
+      device = "/dev/sda1";
+      fsType = "btrfs";
+      options = [ "subvol=@.snapshots" ];
     };
 }
