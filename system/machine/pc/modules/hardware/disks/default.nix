7a524a34026a639f6e1a3159c714f07472cc55a1
llohatca <llohatca@gmail.com>
2025-01-11 18:47:09 +0500

    system/machine/pc/modules/hardware/disks: Improve disk mount
    Signed-off-by: llohatca <llohatca@gmail.com>

--- system/machine/pc/modules/hardware/disks/default.nix
+++ system/machine/pc/modules/hardware/disks/default.nix
@@ -3,7 +3,7 @@
     {
       device = "/dev/sda1";
       fsType = "btrfs";
-      options = [ "subvol=@" ];
+      options = [ "subvol=@" "rw" "relatime" "max_inline=256" "commit=300" "autodefrag" ];
     };
 
 
@@ -11,28 +11,28 @@
     {
       device = "/dev/sda1";
       fsType = "btrfs";
-      options = [ "subvol=@home" ];
+      options = [ "subvol=@home" "rw" "relatime" "max_inline=256" "commit=300" "autodefrag" ];
     };
 
   fileSystems."/var/log" =
     {
       device = "/dev/sda1";
       fsType = "btrfs";
-      options = [ "subvol=@log" ];
+      options = [ "subvol=@log" "noatime" "rw" "relatime" "max_inline=256" "commit=300" "autodefrag" ];
     };
 
   fileSystems."/nix" =
     {
       device = "/dev/sda1";
       fsType = "btrfs";
-      options = [ "subvol=@nix" ];
+      options = [ "subvol=@nix" "noatime" "rw" "relatime" "max_inline=256" "commit=300" "autodefrag" ];
     };
 
   fileSystems."/.snapshots" =
     {
       device = "/dev/sda1";
       fsType = "btrfs";
-      options = [ "subvol=@.snapshots" ];
+      options = [ "subvol=@.snapshots" "rw" "relatime" "max_inline=256" "commit=300" "autodefrag" ];
     };
   swapDevices = [
     {
